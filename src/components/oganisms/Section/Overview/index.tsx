f14384dca97a9c996586c94bac0d60e85586901a
longnv99 <litlong99@gmail.com>
2024-03-27 17:36:30 +0700

    feat: add thumbnail overview

--- src/components/oganisms/Section/Overview/index.tsx
+++ src/components/oganisms/Section/Overview/index.tsx
@@ -4,6 +4,8 @@ import styles from "./index.module.scss";
 import cx from "classnames";
 import Button from "@/components/atoms/Button";
 import { motion } from "framer-motion";
+import BgOverview from "@/components/atoms/Images/bh-overview.svg";
+import Image from "next/image";
 
 const Overview: FC = () => {
   const onClick = () => {
@@ -13,34 +15,44 @@ const Overview: FC = () => {
 
   return (
     <section className={styles.section}>
-      <div className={cx("container", styles.content)}>
+      <div className={cx("container", styles.wrapper)}>
+        <div className={styles.content}>
+          <motion.div
+            initial={{ opacity: 0, y: 100 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+          >
+            <h4 className={styles.h4}>Hi,</h4>
+            <h1 className={styles.h1}>
+              {`I'm`}
+              <span className={styles.highlight}>Long</span>
+            </h1>
+          </motion.div>
+          <motion.h2
+            className={styles.h2}
+            initial={{ opacity: 0, x: 250 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6 }}
+          >
+            Front-end Developer
+          </motion.h2>
+          <motion.div
+            initial={{ opacity: 0, x: -200 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6 }}
+          >
+            <Button className={styles.button} onClick={onClick}>
+              About me
+            </Button>
+          </motion.div>
+        </div>
         <motion.div
-          initial={{ opacity: 0, y: 100 }}
+          initial={{ opacity: 0, y: 130 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8 }}
+          className={styles.thumbnail}
         >
-          <h4 className={styles.h4}>Hi,</h4>
-          <h1 className={styles.h1}>
-            {`I'm`}
-            <span className={styles.highlight}>Long</span>
-          </h1>
-        </motion.div>
-        <motion.h2
-          className={styles.h2}
-          initial={{ opacity: 0, x: 250 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.6 }}
-        >
-          Front-end Developer
-        </motion.h2>
-        <motion.div
-          initial={{ opacity: 0, x: -200 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.6 }}
-        >
-          <Button className={styles.button} onClick={onClick}>
-            About me
-          </Button>
+          <Image src={BgOverview} alt="bg-overview" />
         </motion.div>
       </div>
       <BackgroundMotion />
